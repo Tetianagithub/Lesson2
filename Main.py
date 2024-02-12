e58397b2bae8a5bff2ff14f1871d4801f620e105
Tanya <opttorgshop@gmail.com>
2024-02-12 23:31:36 +0000

    Example-2 has been implemented

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+meters = int(input("enter the number of meters: "))
+operation = input("Enter 'miles', 'inches', или 'yards': ")
+if operation == 'miles':
+    result = (meters / 1609)
+    print("miles:", result)
+elif operation == 'inches':
+    result = (meters * 39.37)
+    print("inches:", result)
+elif operation == 'yards':
+    result = (meters * 1.094)
+    print("yards:", result)
+else:
+    print("Invalid operation. Please enter 'miles', 'inches', or 'yards'.")
